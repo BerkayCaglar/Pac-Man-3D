74e44acc76d47d0eb36de8dff9d0c37de8fe1ab4
Berkay ÇAĞLAR <berkay_caglar123@hotmail.com>
2022-05-21 01:09:12 +0300

    Updated

--- PacmanFPS/Assets/Scripts/NavMeshh.cs
+++ PacmanFPS/Assets/Scripts/NavMeshh.cs
@@ -1,18 +1,13 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.AI;
 public class NavMeshh : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
+    [SerializeField]
+    private Transform target;
     void Update()
     {
-        
+        GetComponent<NavMeshAgent>().destination = target.position;
     }
 }
